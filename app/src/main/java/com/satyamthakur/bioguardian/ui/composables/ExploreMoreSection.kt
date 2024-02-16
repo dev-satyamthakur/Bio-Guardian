e9b6841d68b12ec3558cf569f844a239d8590eea
Satyam Thakur <sat.thakur20@gmail.com>
2024-02-16 22:45:04 +0530

    moving explore more section to a row

--- app/src/main/java/com/satyamthakur/bioguardian/ui/composables/ExploreMoreSection.kt
+++ app/src/main/java/com/satyamthakur/bioguardian/ui/composables/ExploreMoreSection.kt
@@ -2,12 +2,8 @@ package com.satyamthakur.bioguardian.ui.composables
 
 import android.content.Intent
 import android.net.Uri
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,33 +12,20 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowRight
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat.startActivity
 import com.satyamthakur.bioguardian.R
 import com.satyamthakur.bioguardian.ui.theme.Montserrat
 import com.satyamthakur.bioguardian.ui.theme.md_theme_light_onTertiaryContainer
@@ -63,23 +46,21 @@ fun ExploreMoreSection() {
         color = md_theme_light_onTertiaryContainer
     )
     Spacer(modifier = Modifier.height(10.dp))
-    LazyRow(
-        contentPadding = PaddingValues(start = 16.dp, end = 16.dp),
-        horizontalArrangement = Arrangement.spacedBy(10.dp)
+    Row(
+        modifier = Modifier.fillMaxWidth()
     ) {
-        itemsIndexed(exploreTitles) { index, title ->
-            ExploreMoreCard(title = title, index)
-        }
+        ExploreMoreCard(title = exploreTitles[0], index = 0, modifier = Modifier.weight(1f).padding(start = 16.dp, top = 10.dp, end = 5.dp))
+        ExploreMoreCard(title = exploreTitles[1], index = 1, modifier = Modifier.weight(1f).padding(end = 16.dp, top = 10.dp, start = 5.dp))
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ExploreMoreCard(title: String, index: Int) {
+fun ExploreMoreCard(title: String, index: Int, modifier: Modifier) {
     val context = LocalContext.current
     val conservationEffortsIntent = remember { Intent(Intent.ACTION_VIEW, Uri.parse("https://www.worldwildlife.org/initiatives/wildlife-conservation")) }
     Card(
-        modifier = Modifier.size(154.dp),
+        modifier = modifier,
         colors = CardDefaults.cardColors(
             containerColor = md_theme_light_tertiaryContainer,
         ),
@@ -105,8 +86,8 @@ fun ExploreMoreCard(title: String, index: Int) {
                 text = title,
                 fontFamily = Montserrat,
                 fontWeight = FontWeight.SemiBold,
-                fontSize = 16.sp,
-                lineHeight = 24.sp,
+                fontSize = 14.sp,
+                lineHeight = 22.sp,
                 color = md_theme_light_onTertiaryContainer
             )
             Spacer(modifier = Modifier.weight(1f))
@@ -125,7 +106,7 @@ fun ExploreMoreCard(title: String, index: Int) {
 @Preview()
 @Composable
 fun PrevExplore() {
-    ExploreMoreCard("Nearby\nReserves", 0)
+    ExploreMoreCard("Nearby\nReserves", 0, Modifier)
 }
 
 @Preview(showBackground = true)
